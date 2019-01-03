41c4c2445d9390dfef9d8793cca40668b3d1346b
xfournet <xfournet@users.noreply.github.com>
2019-01-03 17:27:08 +0100

    Fix javadoc warnings

--- jconfig/src/main/java/io/github/xfournet/jconfig/FileContentHandler.java
+++ jconfig/src/main/java/io/github/xfournet/jconfig/FileContentHandler.java
@@ -15,6 +15,7 @@ public interface FileContentHandler {
      * @param source the input where to read the source of the apply process
      * @param result the output where to write the result of the apply process
      * @param diff the {@link Diff} object to be applied
+     * @throws IOException in case an error occurs on a stream
      */
     default void apply(@Nullable InputStream source, OutputStream result, Diff diff) throws IOException {
         throw new UnsupportedOperationException();
@@ -26,6 +27,7 @@ public interface FileContentHandler {
      * @param source the content from which to generate the diff
      * @param referenceSource the reference file to be used for the diff process
      * @return {@code null} if files are identical, else return the {@link Diff} object that can be then replayed with {@link #apply(InputStream, OutputStream, Diff)} method
+     * @throws IOException in case an error occurs on a stream
      */
     @Nullable
     default Diff diff(InputStream source, @Nullable InputStream referenceSource) throws IOException {
@@ -38,6 +40,7 @@ public interface FileContentHandler {
      * @param source1 the first source of the merge that will be merged into the 2nd source
      * @param source2 the second source of the merge into which the first source will be merged
      * @param result the output where to write the result of the merge
+     * @throws IOException in case an error occurs on a stream
      */
     default void merge(InputStream source1, InputStream source2, OutputStream result) throws IOException {
         throw new UnsupportedOperationException();
@@ -49,6 +52,7 @@ public interface FileContentHandler {
      * @param source the input where to read the source of the update process
      * @param result the output where to write the result of the update process
      * @param entries the list of entries to be updated
+     * @throws IOException in case an error occurs on a stream
      */
     default void setEntries(InputStream source, OutputStream result, List<String> entries) throws IOException {
         throw new UnsupportedOperationException();
@@ -60,6 +64,7 @@ public interface FileContentHandler {
      * @param source the input where to read the source of the remove process
      * @param result the output where to write the result of the remove process
      * @param entries the list of entries to be updated
+     * @throws IOException in case an error occurs on a stream
      */
     default void removeEntries(InputStream source, OutputStream result, List<String> entries) throws IOException {
         throw new UnsupportedOperationException();
