e96121b776ba8567827433bdd0a64f984a473bb4
xfournet <xfournet@users.noreply.github.com>
2019-04-05 16:55:40 +0200

    Update GPG keys configuration

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Configuring maven setting.xml"
+echo "Configuring Maven setting.xml"
 mkdir -p ~/.m2
 cp ci/settings.xml ~/.m2/settings.xml
+
+if [ -n "$GPG_SECRET_KEYS" -a -n "$GPG_OWNERTRUST" ]; then
+    echo
+    echo "Configuring GPG keys"
+    echo "$GPG_SECRET_KEYS" | base64 --decode | gpg --import
+    echo "$GPG_OWNERTRUST" | base64 --decode | gpg --import-ownertrust
+else
+    echo
+    echo "No GPG keys configured"
+fi
--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -5,15 +5,10 @@ echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
 echo "TRAVIS_TAG = $TRAVIS_TAG"
 echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 
-if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$SONAR_TOKEN" != "" -a "$GPG_KEYNAME" != "" ]; then
+if [ "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" -a "$SONAR_TOKEN" != "" -a "$GPG_SECRET_KEYS" != "" ]; then
     echo
     echo "Build, test and generate Sonar report"
     mvn -B clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
-
-    echo
-    echo "Decrypting GPG files"
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/pubring.gpg.enc -out ci/pubring.gpg
-    openssl aes-256-cbc -d -k "$OPENSSL_PASSPHRASE" -in ci/secring.gpg.enc -out ci/secring.gpg
 
     if [ \("$TRAVIS_BRANCH" = "master" -o "$TRAVIS_TAG" != ""\) -a "$OSSRH_USERNAME" != "" ]; then
         echo
